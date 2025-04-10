744d2edd03290ea48c8fae6ed3513d221727d172
mdgrossi <35579673+mdgrossi@users.noreply.github.com>
2025-04-09 23:46:16 -0400

    Fix typos, add docstrings, add cvmethods

--- scripts/moderncv.py
+++ scripts/moderncv.py
@@ -25,7 +25,7 @@ class Entry():
         ~dada: comma-separated values with which to populate the dataframe. Can  be
         any dtype and any number of items but must be a multiple of `len(columns)`.
         
-        RETJRNS
+        RETURNS
         -------
         Pandas dataframe with columns `columns` filled row-wise with `data`
         """
@@ -41,7 +41,7 @@ class Entry():
         ~dada: comma-separated values with which to populate the dataframe. Can  be
         any dtype and any number of items but must be a multiple of `len(columns)`.
         
-        RETJRNS
+        RETURNS
         -------
         Pandas dataframe with columns `columns` filled row-wise with `data`
         """
@@ -164,7 +164,7 @@ class Education(Entry):
     def __init__(self, columns, *data):
         super().__init__(columns, *data)
     
-    def to_html(self, degree, institution, where, date, award, minor):
+    def to_html(self, degree, institution, location, date, award, minor):
     
         # De-LaTeXify
         tbl = self.entry.copy()
@@ -177,9 +177,9 @@ class Education(Entry):
             table_end = '<tbody>\n<table>\n'
             degreerow = f'<tr>\n<th scope="row"><b>{school[degree]}</b></th>\n<td align="right"><b>{school[date]}</b></td>\n</tr>\n'
             if len(school[award]) > 2:
-                schoolrow = f'<tr>\n<th scope="row"><i>{school[institution]}, {school[award]}</i></th>\n<td align="right"><i>{school[where]}</i></td>\n</tr>\n'
+                schoolrow = f'<tr>\n<th scope="row"><i>{school[institution]}, {school[award]}</i></th>\n<td align="right"><i>{school[location]}</i></td>\n</tr>\n'
             else:
-                schoolrow = f'<tr>\n<th scope="row"><i>{school[institution]}</i></th>\n<td align="right"><i>{school[where]}</i></td>\n</tr>\n'
+                schoolrow = f'<tr>\n<th scope="row"><i>{school[institution]}</i></th>\n<td align="right"><i>{school[location]}</i></td>\n</tr>\n'
             if len(school[minor]) > 2:
                 extrarow = f'<tr>\n<th scope="row" colspan="2">Minor in {school[minor]}</th>\n</tr>\n'
             else:
@@ -189,18 +189,18 @@ class Education(Entry):
             html_table = table_start + degreerow + schoolrow + extrarow + table_end
             print(html_table)
     
-    def to_pdf(self, degree, institution, where, date, award, minor):
+    def to_pdf(self, degree, institution, location, date, award, minor):
         
         # Get entry data
         tbl = self.entry.copy()
 
         # Create LaTeX moderncv `\cventry`` call
-        def create_str(tbl, degree, institution, where, date, award, minor):
+        def create_str(tbl, degree, institution, location, date, award, minor):
             """Create LaTeX moderncv `\cventry` call"""
             command_start = '\\cventry'
             command_end = '\\vspace{0.5em}'
             print(
-                command_start + '{' + tbl[where] + '}' +\
+                command_start + '{' + tbl[location] + '}' +\
                 '{' + tbl[institution] + '}' +\
                 '{' + tbl[degree] + '}' +\
                 '{' + tbl[date] + '}' +\
@@ -210,14 +210,14 @@ class Education(Entry):
             )
         
         # Replicate for all jobs
-        [create_str(row, degree, institution, where, date, award, minor) for _, row in tbl.iterrows()]
+        [create_str(row, degree, institution, location, date, award, minor) for _, row in tbl.iterrows()]
 
 class Experience(Entry):
     
     def __init__(self, columns, *data):
         super().__init__(columns, *data)
     
-    def to_html(self, employer, role, dates, where, desc):
+    def to_html(self, employer, role, dates, location, desc):
     
         # Get enrty data
         tbl = self.entry.copy()
@@ -237,23 +237,23 @@ class Experience(Entry):
         for _, job in tbl.iterrows():
             # Building blocks
             table_start = '<table class = "mytable">\n<tbody>\n'
-            table_end = '<tbody>\n<table>\n'
+            table_end = '</tbody>\n</table>\n'
             employerrow = f'<tr>\n<th scope="row" colspan="2"><b>{job[employer]}</b></th>\n'
-            jobrow = f'<tr>\n<th scope="row"><i>{job[role]}, {job[where]}</i></th>\n<td align="right"><i>{job[dates]}</i></td>\n</tr>\n'
+            jobrow = f'<tr>\n<th scope="row"><i>{job[role]}, {job[location]}</i></th>\n<td align="right"><i>{job[dates]}</i></td>\n</tr>\n'
             descriptionrow = f'<tr>\n<th scope="row" colspan="2">{job[desc]}</th>\n</tr>\n'
 
             # Assemble
             html_table = table_start + employerrow + jobrow + descriptionrow + table_end
             print(html_table)
     
-    def to_pdf(self, employer, role, dates, where, desc):
+    def to_pdf(self, employer, role, dates, location, desc):
 
         # Replace * with LaTeX list items
         tbl = self.entry.copy()
         tbl[desc] = tbl[desc].apply(lambda x: x.replace('* ', '\\item '))
         
         # Create LaTeX moderncv `\cventry`` call
-        def create_str(tbl, role, employer, where, dates, desc):
+        def create_str(tbl, role, employer, location, dates, desc):
             """Create LaTeX moderncv `\cventry` call"""
             command_start = '\\cventry[\\cvspace]'
             command_end = '\\vspace{0.5em}'
@@ -262,13 +262,13 @@ class Experience(Entry):
                 '{' + tbl[role] + '}' +\
                 '{' + tbl[employer] + '}' +\
                 '{}' +\
-                '{' + tbl[where] + '}' +\
+                '{' + tbl[location] + '}' +\
                 '{' + '\\vspace{0.25em}\\begin{wideitemize}' + tbl[desc] + '\\end{wideitemize}' + '}' +\
                 command_end
             )
         
         # Replicate for all jobs
-        [create_str(row, role, employer, where, dates, desc) for _, row in tbl.iterrows()]
+        [create_str(row, role, employer, location, dates, desc) for _, row in tbl.iterrows()]
         
 class Awards(Entry):
     
@@ -304,6 +304,18 @@ class Awards(Entry):
 # =============================================================================
 
 def list_html(items):
+    """Generate html syntax for a list of values. Note that this returns a
+    string of html code needed to create a table, not a table itself. This is
+    used within a Quarto markdown files to display a table when that file is
+    rendered.
+    
+    ARGUMENTS
+    ---------
+    items: list of dictionaries containing the table entries as values. 
+        Normally, this dictionary would be from the YAML front matter of a
+        Quarto markdown file.
+    """
+    
     list_start = "<html><ul>"
     list_end = "</ul></html>"
     key, _ = list(items[0].items())[0]
@@ -312,14 +324,43 @@ def list_html(items):
     display(Markdown(html_list))
 
 def mdlist(items):
+    """Generate markdown syntax for a list of values. Note that this returns a
+    string of markdown code needed to create a table, not a table itself. This
+    is used within a Quarto markdown files to display a table when that file is
+    rendered.
+    
+    ARGUMENTS
+    ---------
+    items: list of dictionaries containing the table entries as values. 
+        Normally, this dictionary would be from the YAML front matter of a
+        Quarto markdown file.
+    """
+    
     key, _ = list(items[0].items())[0]
     items = "".join(f"- {i[key]}\n" for i in items)
     display(Markdown(items))
 
 def mdskills(items, ncol=2, icons=True):
-    """Create a Markdown list with `ncol` columns from contents of dictionary
-    `items`. If `icons = True`, each skill will be prefaced by an icon if
-    if available (HTML only).
+    """Generate markdown syntax for a list with `ncol` columns from contents of 
+    dict `items` as described below. Note that this returns a string of
+    code needed to create a table, not a table itself. This is used within a
+    Quarto markdown files to display a table when that file is rendered.
+    
+    ARGUMENTS
+    ---------
+    items: list of dictionaries each containing the following items:
+        {
+        'skill': str, the name of the skill to be displayed,
+        'years': str, the number of years of experience to be displayed,
+        'scale': int, a value from 1-10 indicating the degree of experience or
+        expertise for the given skill.
+        }
+        Normally, this dictionary would be from the YAML front matter of a
+        Quarto markdown file.
+    ncols: int, the number of columns the skills table should be arranged in
+    icons: Bool, if True, each skill will be prefaced by an appropriate icon,
+        if one is available for that skill (icons display in html rendering
+        only). Defaults to True.
     """
     
     # Column configuration
@@ -365,37 +406,43 @@ def mdskills(items, ncol=2, icons=True):
     display(Markdown(':::'))
     display(Markdown('::::'))
 
-def mdeducation_table(items):
-    abbr = dict({
-        'Bachelor of Science': 'B.S.',
-        'Master of Science': 'M.S.',
-        'Doctor of Philosophy': 'Ph.D.'
-    })
-
-    def md_table(entry):
-        display(Markdown(f'|<br>**{abbr[entry["degree"]]}, {entry["major"]}** | <br> **{entry["date"]}** |'))
-        if 'extra' in entry.keys():
-            display(Markdown(f'| *{entry["institution"]}, {entry["extra"]}* | *{entry["location"]}* |'))
-        else:
-            display(Markdown(f'| *{entry["institution"]}* | *{entry["location"]}* |'))
-        if 'minor' in entry.keys():
-            display(Markdown(f'| Minor in {entry["minor"]} |  |'))
-        # display(Markdown(' '))
+def mdeducation(items, abbrev=True):
+    """Generate markdown syntax for a list of degrees from contents of
+    dict `items` as described below. Note that this returns a string of
+    code needed to create a table, not a table itself. This is used within a
+    Quarto markdown files to display a table when that file is rendered.
     
-    display(Markdown('| | |'))
-    display(Markdown('| :-------------- | ---: |'))
-    for item in items:
-        md_table(entry=item)
-
-def mdeducation(items):
+    ARGUMENTS
+    ---------
+    items: list of dictionaries each containing the following items:
+        {
+        'degree': str, the type of degree (e.g., 'B.A.', 'Master of Science'),
+        'major': str, the name of the degree,
+        'institution': str, the name of the institution from which the degree
+        was earned,
+        'location': str, location of the institution,
+        'date': str, the date of degree conferral,
+        'minor': str (optional), the name(s) of any minor earned,
+        'extra': str (optional), any other relevant information (e.g., award)
+        }
+        Normally, this dictionary would be from the YAML front matter of a
+        Quarto markdown file.
+    abbrev: Boolean, whether or not to always abbreviate degree type. Defaults
+        to True.
+    """    
     abbr = dict({
         'Bachelor of Science': 'B.S.',
+        'Bachelor of Arts': 'B.A.',
         'Master of Science': 'M.S.',
+        'Master of Arts': 'M.A.',
         'Doctor of Philosophy': 'Ph.D.'
     })
 
     for item in items:
-        display(Markdown(f'**{abbr[item["degree"]]}, {item["major"]}**  '))
+        if abbrev:
+            display(Markdown(f'**{abbr[item["degree"]]}, {item["major"]}**  '))
+        else:
+            display(Markdown(f'**{item["degree"]}, {item["major"]}**  '))
         if 'extra' in item.keys():
             display(Markdown(f'*{item["institution"]}, {item["location"]}* ({item["extra"]})  '))
         else:
@@ -405,6 +452,27 @@ def mdeducation(items):
         display(Markdown(' '))
 
 def mdexperience(items):
+    """Generate markdown syntax for a list of jobs from contents of dict
+    `items` as described below. Note that this returns a string of code needed
+    to create a table, not a table itself. This is used within a Quarto
+    markdown files to display a table when that file is rendered.
+    
+    ARGUMENTS
+    ---------
+    items: list of dictionaries each containing the following items:
+        {
+        'role': str, job title,
+        'employer': str, name of employer,
+        'dates': str, dates of employment,
+        'location': str, location of employment
+        'details': str, description of responsibilities. Multiple 
+        responsibilities can be entered on separate lines prefaced by an 
+        asterisk ('*'), which will render as list.
+        'extra': str (optional), any other relevant information
+        }
+        Normally, this dictionary would be from the YAML front matter of a
+        Quarto markdown file.
+    """    
 
     # Remove pdf page breaks
     ind = [i for i,d in enumerate(items) if 'role' not in d.keys()]
@@ -414,20 +482,129 @@ def mdexperience(items):
 
     for item in items:
         # Add missing elements as placeholders
-        for k in ['role', 'employer', 'where', 'dates', 'details']:
+        for k in ['role', 'employer', 'location', 'dates', 'details']:
             if k not in item.keys():
                 item[k] = ''
 
         display(Markdown('|  |  |'))
         display(Markdown('| :--- | ---: |'))
-        if item['employer'] == '' and item['where'] == '':
+        if item['employer'] == '' and item['location'] == '':
             if 'extra' in item.keys():
                 display(Markdown(f'| *{item["role"]}, {item["extra"]}* | *{item["dates"]}* |\n'.replace('****', '')))
             else:
                 display(Markdown(f'| *{item["role"]}* | *{item["dates"]}* |\n'.replace('****', '')))    
         else:
             if 'extra' in item.keys():
-                display(Markdown(f'| **{item["employer"]}** <br> *{item["role"]}, {item["extra"]}* | **{item["where"]}** <br> *{item["dates"]}* |\n'.replace('****', '')))
+                display(Markdown(f'| **{item["employer"]}** <br> *{item["role"]}, {item["extra"]}* | **{item["location"]}** <br> *{item["dates"]}* |\n'.replace('****', '')))
             else:
-                display(Markdown(f'| **{item["employer"]}** <br> *{item["role"]}* | **{item["where"]}** <br> *{item["dates"]}* |\n'.replace('****', '')))
+                display(Markdown(f'| **{item["employer"]}** <br> *{item["role"]}* | **{item["location"]}** <br> *{item["dates"]}* |\n'.replace('****', '')))
         display(Markdown(item["details"]))
+
+def cventry(items):
+    """Generates markdown syntax for an html table creating a CV entry with
+    date on left and entry description on right. Note that this returns a
+    string of code needed to create a table, not a table itself. This is 
+    used within a Quarto markdown files to display a table when that file is
+    rendered.
+    
+    ARGUMENTS
+    ---------
+    items: list of dictionaries containing each of the following:
+        {
+        'entry': str, description of activity, experience, accomplishment, etc.
+        'year': str, year(s) for entry
+        }
+        Normally, this dictionary would be from the YAML front matter of a
+        Quarto markdown file.
+    """
+    table_start = '<table class = "mytable">\n<tbody>\n'
+    entries = "".join(f"<tr><td class='year'><b>{item['date']}</b></td><td>{item['entry']}</td></tr>" for item in items)
+    table_end = '</tbody>\n</table>\n'
+    
+    # Assemble table
+    html_table = table_start + entries + table_end
+    display(Markdown(html_table))
+    
+def cvexperience(items):
+    """Generate markdown syntax for a CV list of jobs from contents of dict
+    `items` as described below. Note that this returns a string of code needed
+    to create a table, not a table itself. This is used within a Quarto
+    markdown files to display a table when that file is rendered.
+    
+    ARGUMENTS
+    ---------
+    items: list of dictionaries each containing the following items:
+        {
+        'role': str, job title,
+        'employer': str, name of employer,
+        'dates': str, dates of employment,
+        'location': str, location of employment
+        'extra': str (optional), any other relevant information
+        }
+        Normally, this dictionary would be from the YAML front matter of a
+        Quarto markdown file.
+    """    
+
+    table_start = '<table class = "mytable">\n<tbody>\n'
+    entries = "".join(f"<tr><td class='year'><b>{item['dates']}</b></td><td><b>{item['role']}</b> {item['extra']}<br>{item['employer']}, {item['location']}</td></tr>" for item in items)
+    table_end = '</tbody>\n</table>\n'
+    
+    # Assemble table
+    html_table = table_start + entries + table_end
+    display(Markdown(html_table))
+
+def cveducation(items, abbrev=True):
+    """Generate html syntax for a CV list of degrees from contents of dict
+    `items` as described below. Note that this returns a string of code needed
+    to create a table, not a table itself. This is used within a Quarto
+    markdown files to display a table when that file is rendered.
+    
+    ARGUMENTS
+    ---------
+    items: list of dictionaries each containing the following items:
+        {
+        'degree': str, the type of degree (e.g., 'B.A.', 'Master of Science'),
+        'major': str, the name of the degree,
+        'institution': str, the name of the institution from which the degree
+        was earned,
+        'location': str, location of the institution,
+        'date': str, the date of degree conferral,
+        'minor': str (optional), the name(s) of any minor earned,
+        'extra': str (optional), any other relevant information (e.g., award)
+        }
+        Normally, this dictionary would be from the YAML front matter of a
+        Quarto markdown file.
+    abbrev: Boolean, whether or not to always abbreviate degree type. Defaults
+        to True.
+    """    
+    abbr = dict({
+        'Bachelor of Science': 'B.S.',
+        'Bachelor of Arts': 'B.A.',
+        'Master of Science': 'M.S.',
+        'Master of Arts': 'M.A.',
+        'Doctor of Philosophy': 'Ph.D.'
+    })
+    if abbrev:
+        for item in items:
+            item['degree'] = abbr[item['degree']]
+
+    table_start = '<table class = "mytable">\n<tbody>\n'
+    entries = ""
+    for item in items:
+        if 'minor' in item.keys():
+            if 'extra' in item.keys():
+                entries += f"<tr><td class='year'><b>{item['date']}</b></td><td><b>{item['degree']}</b>, {item['major']}, {item['institution']}, {item['location']}<br>Minor: {item['minor']} | *{item['extra']}*</td></tr>"            
+            else:
+                entries += f"<tr><td class='year'><b>{item['date']}</b></td><td><b>{item['degree']}</b>, {item['major']}, {item['institution']}, {item['location']}<br>Minor: {item['minor']}</td></tr>"
+        elif 'extra' in item.keys():
+            if 'minor' in item.keys():
+                entries += f"<tr><td class='year'><b>{item['date']}</b></td><td><b>{item['degree']}</b>, {item['major']}, {item['institution']}, {item['location']}<br>Minor: {item['minor']} | *{item['extra']}*</td></tr>"
+            else:
+                entries += f"<tr><td class='year'><b>{item['date']}</b></td><td><b>{item['degree']}</b>, {item['major']}, {item['institution']}, {item['location']}<br>*{item['extra']}*</td></tr>"                
+        else:
+            entries += f"<tr><td class='year'><b>{item['date']}</b></td><td><b>{item['degree']}</b>, {item['major']}, {item['institution']}, {item['location']}</td></tr>"
+    table_end = '</tbody>\n</table>\n'
+    
+    # Assemble table
+    html_table = table_start + entries + table_end
+    display(Markdown(html_table))
